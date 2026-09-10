5d639da6436441863f8501d7b0b37e86eb98f8b2
Sirusoid <djfedelio@gmail.com>
2026-09-10 17:32:46 +0500

    В настройках в разделе "Пользователи" были внесены изменения. Блок "Добавить пользователя" был обновлен. Сделали его более компактным. 	modified:   assets/css/settings.css 	modified:   settings/users.php

--- settings/users.php
+++ settings/users.php
@@ -156,7 +156,7 @@ require __DIR__ . '/../includes/header.php';
 require __DIR__ . '/../includes/panel.php';
 ?>
 
-<div class="settings-shell">
+<div class="settings-shell settings-users-page">
 	<?php if ($saveSuccess): ?>
 		<div class="alert settings-alert settings-alert--success"><?= h($saveSuccess) ?></div>
 	<?php endif; ?>
