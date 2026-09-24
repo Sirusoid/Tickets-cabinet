e07a7f1e34548a8c820fe68e6928408ae01ece75
Sirusoid <djfedelio@gmail.com>
2026-09-24 17:05:25 +0500

    Update ticket view: added hall name from DB and improved layout

--- sql/migration_order_email_sent.sql
+++
@@ -1,3 +0,0 @@
--- Точечная миграция: не импортировать полный SQL-дамп в production.
-ALTER TABLE payment_sessions
-    ADD COLUMN IF NOT EXISTS order_email_sent_at DATETIME NULL AFTER customer_email;
